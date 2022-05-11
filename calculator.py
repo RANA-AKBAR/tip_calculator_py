3692770cd4dae65094b6e8b64abd9fad28883d0a
rana-akbar <rana.akbar.08@gmail.com>
2022-05-11 19:24:22 +0100

    corrections made

--- calculator.py
+++ calculator.py
@@ -13,10 +13,10 @@ total_bill = float(input("What was the total bill? $"))
 tip_percentage = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
 num_of_people = int(input("How many people to split the bill? "))
 total_with_tip = (total_bill * (1 + tip_percentage/100))
-bill_per_person = round(total_with_tip / num_of_people, 2)
+bill_per_person = round(total_with_tip / num_of_people,2)
+final_amount = "{:.2f}".format(bill_per_person) ##### This makes it 2 decimal places
 
 
 
 
-
-print(f"Each person should pay: ${bill_per_person}")
+print(f"Each person should pay: ${final_amount}")
